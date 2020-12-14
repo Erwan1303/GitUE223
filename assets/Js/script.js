1a84e8d648407817333ff096ae42fcec727dae9f
Erwan1303 <58224807+Erwan1303@users.noreply.github.com>
2020-12-14 12:23:21 +0100

    ajout image principale/bouton/grille

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,3 +1,27 @@
+element = document.getElementById("btn_start");
+element.addEventListener('click', function() 
+
+        {
+				alert ("coucou");
+		});
 
 
-        
+        element = document.getElementById("case1");
+element.addEventListener('click', function() 
+
+        {
+                alert ("coucou");
+                var case1 = document.getElementById("case1");
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function (){
+					console.log(this);
+					if (this.readyState == 4 && this.status == 200){
+						case1.innerHTML = this.responseText;
+					}
+				};
+
+				xhr.open("GET", "assets/Php/Phpfile.php", true);
+				xhr.responseText = "text";
+				xhr.send();
+                
+		});
---
+++ assets/Php/Phpfile.php
@@ -0,0 +1,2 @@
+<?php
+echo '<img src="assets/img/38-11.jpg" alt="image principale"/>';
